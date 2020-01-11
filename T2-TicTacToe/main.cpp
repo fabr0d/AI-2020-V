b8555242954103e15fc9461487ed96f3a80525ef
npc <fabrodaqp@gmail.com>
2020-01-10 20:13:47 -0500

    Code

---
+++ T2-TicTacToe/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+class Node
+{
+public:
+	Node();
+	~Node();
+
+private:
+
+};
+
+Node::Node()
+{
+}
+
+Node::~Node()
+{
+}
