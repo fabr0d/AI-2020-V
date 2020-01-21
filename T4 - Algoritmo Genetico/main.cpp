95fc40e7ea4a7b6a34252d80231de14194d8a9e6
npc <fabrodaqp@gmail.com>
2020-01-21 18:56:14 -0500

    new

---
+++ T4 - Algoritmo Genetico/main.cpp
@@ -0,0 +1,20 @@
+#define GLUT_DISABLE_ATEXIT_HACK
+#include <windows.h>
+#include <math.h>
+#include "GL/glut.h"
+#include <iostream>
+#include <vector>
+#include <random>
+#include <time.h>
+#include <queue>
+#include <stack>
+#include <math.h>
+#define KEY_ESC 27
+using namespace std;
+
+
+
+int main()
+{
+	return 1;
+}
